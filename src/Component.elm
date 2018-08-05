9cac323718b9789448cd02d2282d531a1bfe74a5
brandly <brandly13@gmail.com>
2018-08-05 16:02:18 -0400

    add menu for picking 1p/2p

--- src/Component.elm
+++ src/Component.elm
@@ -35,3 +35,17 @@ raiseOutMsg update toModel toMsg result =
                     update msg2 (toModel model_)
             in
                 ( model__, Cmd.batch [ Cmd.map toMsg cmd1, cmd2 ], msg3 )
+
+
+mapSimple :
+    (msg2 -> model2 -> ( model2, Cmd msg2 ))
+    -> (model1 -> model2)
+    -> (msg1 -> msg2)
+    -> ( model1, Cmd msg1 )
+    -> ( model2, Cmd msg2 )
+mapSimple update toModel toMsg result =
+    let
+        ( state, cmd ) =
+            result
+    in
+        ( toModel state, Cmd.map toMsg cmd )
--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,66 @@
 module Main exposing (..)
 
-import Html exposing (Html, h1, text, div)
+import Html exposing (Html, h1, text, div, button)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Component
 import OnePlayer
 
 
-main : Program Never OnePlayer.Model OnePlayer.Msg
+main : Program Never Model Msg
 main =
     Html.program
-        { init = OnePlayer.init
-        , update = OnePlayer.update
+        { init = ( Selecting, Cmd.none )
+        , update = update
         , view = view
-        , subscriptions = OnePlayer.subscriptions
+        , subscriptions = subscriptions
         }
 
 
-view : OnePlayer.Model -> Html OnePlayer.Msg
+type Model
+    = Selecting
+    | One OnePlayer.Model
+    | Two
+
+
+type Msg
+    = OneMsg OnePlayer.Msg
+    | TwoMsg
+    | PlayOne
+    | PlayTwo
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model of
+        One state ->
+            OnePlayer.subscriptions state
+                |> Sub.map OneMsg
+
+        _ ->
+            Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case ( model, msg ) of
+        ( Selecting, PlayOne ) ->
+            OnePlayer.init
+                |> Tuple.mapFirst One
+                |> Tuple.mapSecond (Cmd.map OneMsg)
+
+        ( Selecting, PlayTwo ) ->
+            ( Two, Cmd.none )
+
+        ( One state, OneMsg msg ) ->
+            OnePlayer.update msg state
+                |> Component.mapSimple update One OneMsg
+
+        ( _, _ ) ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
 view model =
     div
         [ style
@@ -25,5 +70,23 @@ view model =
             ]
         ]
         [ h1 [] [ text "dr. mario 💊" ]
-        , OnePlayer.view model
+        , case model of
+            Selecting ->
+                viewSelecting
+
+            One state ->
+                OnePlayer.view state
+                    |> Html.map OneMsg
+
+            Two ->
+                --TwoPlayer.view model
+                text "TODO: build it"
+        ]
+
+
+viewSelecting : Html Msg
+viewSelecting =
+    div []
+        [ button [ onClick PlayOne ] [ text "1p" ]
+        , button [ onClick PlayTwo ] [ text "2p" ]
         ]
